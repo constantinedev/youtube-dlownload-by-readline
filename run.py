964d3b11eeea8e53defda85173d34aa92e7a6380
Constantine Dev <1324252+constantinedev@users.noreply.github.com>
2021-05-03 21:21:03 +0800

    Update run.py

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ file_name = 'links.txt'
 order = 'youtube-dl --write-description --write-info-json --write-annotations --write-sub --write-thumbnail '
 source = open(file_name, 'r')
 for line in source:
-	subprocess.call([order + line], shell=True)
+	subprocess.call([order + line.strap()], shell=True)
 source.close()
