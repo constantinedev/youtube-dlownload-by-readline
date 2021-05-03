2b011cdc846e0be0535d394f78f1d75af72950c0
Constantine Dev <1324252+constantinedev@users.noreply.github.com>
2021-05-03 20:02:50 +0800

    Update run.py

--- run.py
+++ run.py
@@ -4,7 +4,6 @@ file_name = 'links.txt'
 order = 'youtube-dl --write-description --write-info-json --write-annotations --write-sub --write-thumbnail '
 with open(file_name) as fp:
 	line = fp.readline()
-	cnt = 1
 	while line:
 		turl = line.strip()
 		subprocess.call([order + turl], shell=True)
