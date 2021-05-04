fb7be82568d61f10fb529cd1913636aef46abeb8
Constantine Dev <1324252+constantinedev@users.noreply.github.com>
2021-05-05 01:29:38 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,24 @@
 import subprocess
+import threading
 
-file_name = 'links.txt'
-order = 'youtube-dl --write-description --write-info-json --write-annotations --write-sub --write-thumbnail '
+file_name = 'links1.txt'
+order = 'youtube-dl --write-description --write-info-json --write-annotations --write-sub --write-thumbnail --download-archive '
 source = open(file_name, 'r')
-for line in source:
-	subprocess.call([order + line.strap()], shell=True)
+outputdir = '' #<--Input Download location here
+output = ' -o ' + outputdir
+lastIndex = 0
+lines = source.readlines()
+lineCount = len(lines)
+def worker():
+	global lastIndex
+	global lines
+	global lineCount
+	while lastIndex < lineCount:
+		lastIndex +=1
+		line = lines[lastIndex-1]
+		subprocess.call([order + line.strip() + output], shell=True)
+
+threads = [threading.Thread(target=worker) for _i in range(300)]
+for thread in threads:
+	thread.start()
 source.close()
